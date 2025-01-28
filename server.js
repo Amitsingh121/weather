d5e4555157dfaea45156e6415efcb9badd0d326d
Amitsingh121 <amitksingh7779@gmail.com>
2025-01-28 15:04:55 +0530

    first commit

--- server.js
+++ server.js
@@ -7,8 +7,24 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.get('/api/weather', async (req, res) => {
-    const city = req.query.city; 
+// app.get('/api/weather', async (req, res) => {
+//     const city = req.query.city; 
+//     const apiKey = process.env.API_KEY;
+//     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+//     if (!city) {
+//         return res.status(400).json({ error: 'City parameter is required' });
+//     }
+
+//     try {
+//         const response = await axios.get(url);
+//         res.status(200).json(response.data);
+//     } catch (error) {
+//         res.status(500).json({ error: 'Error fetching weather data' });
+//     }
+// });
+module.exports = async (req, res) => {
+    const city = req.query.city;
     const apiKey = process.env.API_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
@@ -22,6 +38,5 @@ app.get('/api/weather', async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Error fetching weather data' });
     }
-});
-
+};
 app.listen(PORT, () => {});
